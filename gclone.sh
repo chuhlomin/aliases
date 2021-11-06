079279024119b96c3e42f5f36d040311a494774f
Konstantin Chukhlomin <mail@chuhlomin.com>
2021-11-06 12:31:57 -0400

    Fix gclone.sh script

--- gclone.sh
+++ gclone.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
+#
+# gclone is a wrapper on top of the `git clone` command, it:
+# 1. parses the URL
+# 2. creates "owner" directory in ~/Projects/
+# 3. clones repository into ~/Projects/<owner>/<repo>
+#
+# Usage:
+# gclone git@github.com:chuhlomin/terraform.git
+# gclone https://github.com/chuhlomin/terraform.git
+#
+# Install:
+# chmod +x gclone.sh
+# mv gclone.sh /usr/local/bin/gclone
+#
+# License: GPL v3.0 https://opensource.org/licenses/GPL-3.0
+# Author: Konstantin Chukhlomin
+# Version: 1.0
 
-# Original work: https://gist.github.com/mihow/9c7f559807069a03e302605691f85572
+PROJECTS_DIR=~/Projects
 
-function e {
-    local filename=$([ -z "$1" ] && echo ".env" || echo "$1");
+IFS=':/.' read -ra ADDR <<< "$1"
 
-    if [ -f $filename ]
-    then
-        set -o allexport;
-        source $filename;
-        set +o allexport;
-    else
-        echo "File $filename not found";
-    fi
-}
+if [ ${#ADDR[@]} == 5 ] # for URLs like git@github.com:chuhlomin/homepage.git
+then
+    OWNER=${ADDR[2]}
+    REPO=${ADDR[3]}
+elif [ ${#ADDR[@]} == 8 ] # for URLs like # https://github.com/chuhlomin/homepage.git
+then
+    OWNER=${ADDR[5]}
+    REPO=${ADDR[6]}
+else
+    echo "Unrecognized repository URL"
+fi
+
+mkdir -p $PROJECTS_DIR/$OWNER
+
+git clone $1 $PROJECTS_DIR/$OWNER/$REPO
